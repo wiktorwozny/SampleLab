aa9a2f355c9ba01f54849ed5f95d63b86bf17328
remekozicki <remigiusz.kozicki@gmail.com>
2024-03-24 14:14:11 +0100

    added backend project

---
+++ slp-backend/src/main/java/com/example/slpbackend/SlpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.slpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlpBackendApplication.class, args);
+    }
+
+}
---
+++ slp-backend/src/test/java/com/example/slpbackend/SlpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
