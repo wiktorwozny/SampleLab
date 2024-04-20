1e0214933b323dc319237f293ea990d8b22d5691
Mateusz Waga <wagulec123@gmail.com>
2024-04-20 12:45:22 +0200

    aplication connected with aws db

--- slp-backend/src/main/java/agh/edu/pl/slpbackend/SlpBackendApplication.java
+++ slp-backend/src/main/java/agh/edu/pl/slpbackend/SlpBackendApplication.java
@@ -2,12 +2,20 @@ package agh.edu.pl.slpbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class SlpBackendApplication {
 
     public static void main(String[] args) {
+        loadEnvVariables();
         SpringApplication.run(SlpBackendApplication.class, args);
     }
-
+    private static void loadEnvVariables() {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.getenv().forEach((k, v) -> {
+            if (dotenv.get(k) != null) {
+                System.setProperty(k, dotenv.get(k));
+            }
+        });
+    }
 }
