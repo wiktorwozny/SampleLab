2f3c0482a8047c2679cc8ed91c4bb763efcb8e09
remekozicki <remigiusz.kozicki@gmail.com>
2024-06-01 19:34:58 +0200

    sidebar almost done, style in tailwind

--- slp-frontend/src/components/Sidebar.tsx
+++ slp-frontend/src/components/Sidebar.tsx
@@ -7,14 +7,28 @@ const Sidebar: React.FC = () => {
 
 
     return (
-        <div className="sidebar sticky">
-            <h2>Menu</h2>
-            <ul>
-                <li><a onClick={() => navigate('/')}>Home</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
+        <div className="sidebar sticky p-15 bg-gray-900 h-screen top-0 w-64 min-w-1/12 ">
+            <div className="m-2">
+                <h2 className="pt-12 pb-4 text-white">Menu</h2>
+                <ul className="list-none p-0">
+                    <li className="my-1.5">
+                        <a
+                            className="text-white no-underline block p-2.5 rounded cursor-pointer hover:bg-gray-600"
+                            onClick={() => navigate('/')}
+                        >Lista próbek</a>
+                    </li>
+                    <li className="my-1.5"><a
+                        className="text-white no-underline block p-2.5 rounded cursor-pointer hover:bg-gray-600"
+                        href="#services">to be done</a></li>
+                    <li className="my-1.5"><a
+                        className="text-white no-underline block p-2.5 rounded cursor-pointer hover:bg-gray-600"
+                        href="#about">to be done</a></li>
+                    <li className="my-1.5"><a
+                        className="text-white no-underline block p-2.5 rounded cursor-pointer hover:bg-gray-600"
+                        href="#contact">to be done</a></li>
+                </ul>
+            </div>
+
         </div>
     );
 };
