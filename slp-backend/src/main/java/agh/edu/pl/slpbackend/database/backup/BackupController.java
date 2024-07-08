e2085d932f62bf48e31dbfd064aeb245437e68db
remekozicki <remigiusz.kozicki@gmail.com>
2024-07-08 21:57:02 +0200

    backup - backend

---
+++ slp-backend/src/main/java/agh/edu/pl/slpbackend/database/backup/BackupController.java
@@ -0,0 +1,32 @@
+package agh.edu.pl.slpbackend.database.backup;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/backup") //TODO odpowiedni rooting jeszcze nie wiem XDD
+@CrossOrigin(origins = "http://localhost:3000")
+public class BackupController {
+
+    private BackupService backupService;
+
+    @GetMapping("/")
+    public ResponseEntity<Void> backup() {
+        try {
+            if (backupService.exportDatabase() != 0) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // nie wiem jaki inny exeption
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ slp-backend/src/main/java/agh/edu/pl/slpbackend/database/backup/BackupService.java
@@ -0,0 +1,60 @@
+package agh.edu.pl.slpbackend.database.backup;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class BackupService {
+
+    public int exportDatabase() throws IOException, InterruptedException {
+        String pgDumpPath = "\"C:\\Program Files\\PostgreSQL\\15\\bin\\pg_dump.exe\"";  // Ścieżka do pg_dump
+        String host = "localhost";
+        String port = "5432";
+        String database = "postgres";
+        String user = "postgres";
+        String password = "Rk020409";
+        String home = System.getProperty("user.home");
+        String backupFile = home + "/Downloads/" + "backup.sql";
+
+        // Tworzenie komendy
+        String[] command = new String[]{
+                pgDumpPath,
+                "--host=" + host,
+                "--port=" + port,
+                "--username=" + user,
+                "--format=plain",
+                "--data-only",
+                "--file=" + backupFile,
+                database
+        };
+
+        try {
+            // Ustawienie zmiennej środowiskowej dla hasła
+            ProcessBuilder pb = new ProcessBuilder(command);
+            pb.environment().put("PGPASSWORD", password);
+            pb.redirectErrorStream(true);
+
+            // Uruchomienie procesu
+            Process process = pb.start();
+
+            // Odczyt wyjścia procesu
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Czekanie na zakończenie procesu
+            return process.waitFor();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
