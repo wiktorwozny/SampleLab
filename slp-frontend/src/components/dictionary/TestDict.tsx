3a03fdea89f2a4a7ecbcb8e0e99d13cb961774c9
remekozicki <remigiusz.kozicki@gmail.com>
2024-08-13 20:49:48 +0200

    dictionary

--- slp-frontend/src/components/dictionary/TestDict.tsx
+++ slp-frontend/src/components/dictionary/TestDict.tsx
@@ -1,41 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {HiAdjustmentsHorizontal} from "react-icons/hi2";
 import Table from "../ui/Table";
+import {getAllClients} from "../../helpers/clientApi";
+import {Address, Client} from "../../utils/types";
 
 
-interface User {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-}
-
-const users: User[] = [
-    {id: 1, name: 'John Doe', age: 28, email: 'john@example.com'},
-    {id: 2, name: 'Jane Smith', age: 34, email: 'jane@example.com'},
-    {id: 3, name: 'Sam Johnson', age: 21, email: 'sam@example.com'},
-];
+const formatAddress = (address: Address): string => {
+    return `${address.street}, ${address.zipCode} ${address.city}`;
+};
 
 const columns = [
     {header: 'ID', accessor: 'id'},
     {header: 'Name', accessor: 'name'},
-    {header: 'Age', accessor: 'age'},
-    {header: 'Email', accessor: 'email'},
+    {header: 'WIJHARS Code', accessor: 'wijharsCode'},
+
 ];
 
 const TestDict = () => {
+    const [clientsList, setClientsList] = useState<Client []>([])
+
+    useEffect(() => {
+        const getClients = async () => {
+            try {
+                let response = await getAllClients();
+                console.log(response.data)
+                if (response.status === 200) {
+                    setClientsList(response.data)
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        getClients();
+    }, []);
+
+
     return (
         <div className="w-full">
             <h1 className="text-center font-bold text-3xl w-full my-3">Test dict</h1>
             <div className="w-full justify-end flex">
                 <div
-                    className="flex border relative mr-2 mb-2 p-2 border-black flex items-center hover:bg-gray-300 cursor-pointer"
+                    className="flex border relative mr-2 mb-2 p-2 border-black items-center hover:bg-gray-300 cursor-pointer"
                 >
                     <div>Filtruj &nbsp;</div>
                     <HiAdjustmentsHorizontal className="text-3xl"></HiAdjustmentsHorizontal>
                 </div>
             </div>
-            <Table<User> columns={columns} data={users}/>
+            <Table<Client> columns={columns} data={clientsList}/>
         </div>);
 };
 
