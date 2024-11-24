55bcaba0f5a232a15eca2ae0723d09231c5d33dc
remekozicki <remigiusz.kozicki@gmail.com>
2024-11-24 18:54:48 +0100

    fixes  pagination and alert popup

--- slp-frontend/src/components/AlertComponent.tsx
+++ slp-frontend/src/components/AlertComponent.tsx
@@ -1,42 +1,42 @@
-import { Collapse, Alert, IconButton, AlertTitle } from "@mui/material";
-import { IoClose } from "react-icons/io5";
-import { AlertContext } from "../contexts/AlertsContext";
-import { useContext, useEffect } from "react";
-import { useRef, RefObject } from "react";
-const AlertComponent = ({isToken = true}:any) => {
+import {Alert, AlertTitle, Collapse, IconButton} from "@mui/material";
+import {IoClose} from "react-icons/io5";
+import {AlertContext} from "../contexts/AlertsContext";
+import {RefObject, useContext, useEffect, useRef} from "react";
+
+const AlertComponent = ({isToken = true}: any) => {
     const {alertDetails, setAlertDetails} = useContext(AlertContext);
     const alertRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-    useEffect(()=>{
-        if(alertDetails.isAlert){
-            setTimeout(()=>{
-                setAlertDetails(prev=>({...prev,isAlert:false}))
-            },10000)
+    useEffect(() => {
+        if (alertDetails.isAlert) {
+            setTimeout(() => {
+                setAlertDetails(prev => ({...prev, isAlert: false}))
+            }, 10000)
             console.log(alertRef.current)
             let width = alertRef.current?.clientWidth
             if (width && alertRef.current) {
-                alertRef.current.style.right = `${width / 2}px`;
+                alertRef.current.style.right = `0`;
             }
         }
-    },[alertDetails])
-    return (<Collapse in = {alertDetails.isAlert} className="w-full flex justify-center">
+    }, [alertDetails])
+    return (<Collapse in={alertDetails.isAlert} className="w-full flex justify-center ">
         <Alert
-            className={`w-fit ${isToken? "relative":""} mx-auto`}
+            className={`w-fit ${isToken ? "relative" : ""} mx-auto right-0`}
             ref={alertRef}
             severity={alertDetails.type}
             action={
                 <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                    setAlertDetails(prev=>({...prev,isAlert:false}))
-                }}
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                        setAlertDetails(prev => ({...prev, isAlert: false}))
+                    }}
                 >
-                    <IoClose fontSize="inherit" />
+                    <IoClose fontSize="inherit"/>
                 </IconButton>
             }
         >
-            <AlertTitle>{alertDetails.type==="success"?"Sukces":"Błąd"}</AlertTitle>
+            <AlertTitle>{alertDetails.type === "success" ? "Sukces" : "Błąd"}</AlertTitle>
             {alertDetails.message}
         </Alert>
     </Collapse>)
