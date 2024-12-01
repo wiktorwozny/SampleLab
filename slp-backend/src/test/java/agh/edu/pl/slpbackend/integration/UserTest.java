209501374bd7f24f9f4855836cebaa5f84e0ce15
Witold Strzeboński <wstrzebo@gmail.com>
2024-12-01 13:54:43 +0100

    add other tests for users

--- slp-backend/src/test/java/agh/edu/pl/slpbackend/integration/UserTest.java
+++ slp-backend/src/test/java/agh/edu/pl/slpbackend/integration/UserTest.java
@@ -2,8 +2,11 @@ package agh.edu.pl.slpbackend.integration;
 
 import agh.edu.pl.slpbackend.controller.UserController;
 import agh.edu.pl.slpbackend.dto.UserDto;
+import agh.edu.pl.slpbackend.dto.users.LoginRequest;
 import agh.edu.pl.slpbackend.enums.RoleEnum;
 import agh.edu.pl.slpbackend.exception.AccountAlreadyExistsException;
+import agh.edu.pl.slpbackend.exception.UserNotFoundException;
+import agh.edu.pl.slpbackend.exception.WrongPasswordException;
 import agh.edu.pl.slpbackend.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
@@ -27,7 +30,7 @@ public class UserTest {
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    public void register() {
+    void register() {
         var userDto = UserDto.builder()
                 .name("Adam Nowak")
                 .email("nowak@gmail.com")
@@ -52,7 +55,7 @@ public class UserTest {
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    public void register_fails_when_email_taken() {
+    void register_fails_when_email_taken() {
         var userCount = repository.count();
         var userDto = UserDto.builder()
                 .name("Adam Nowak")
@@ -63,5 +66,69 @@ public class UserTest {
         assertThatThrownBy(() -> controller.register(userDto))
                 .isInstanceOf(AccountAlreadyExistsException.class);
         assertThat(repository.count()).isEqualTo(userCount);
+    }
+
+    @Test
+    void login() {
+        var loginRequest = new LoginRequest("worker@gmail.com", "worker");
+        var response = controller.login(loginRequest);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        var loginResponse = response.getBody();
+        assertThat(loginResponse).isNotNull();
+        assertThat(loginResponse.token()).isNotEmpty();
+
+        var user = repository.findByEmail(loginRequest.email()).orElse(null);
+        assertThat(user).isNotNull();
+        assertThat(loginResponse.user().getName()).isEqualTo(user.getName());
+        assertThat(loginResponse.user().getRole()).isEqualTo(user.getRole());
+    }
+
+    @Test
+    void login_fails_when_unknown_email() {
+        var loginRequest = new LoginRequest("", "");
+
+        assertThatThrownBy(() -> controller.login(loginRequest))
+                .isInstanceOf(UserNotFoundException.class);
+    }
+
+    @Test
+    void login_fails_when_wrong_password() {
+        var loginRequest = new LoginRequest("worker@gmail.com", "");
+
+        assertThatThrownBy(() -> controller.login(loginRequest))
+                .isInstanceOf(WrongPasswordException.class);
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void getAll() {
+        var response = controller.getUsers();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        var users = response.getBody();
+        assertThat(users).isNotNull();
+        assertThat(users.size()).isEqualTo(repository.count());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void delete() {
+        var email = "worker@gmail.com";
+        assertThat(repository.existsByEmail(email)).isTrue();
+        var count = repository.count();
+
+        controller.deleteUser(email);
+
+        assertThat(repository.existsByEmail(email)).isFalse();
+        assertThat(repository.count()).isEqualTo(count - 1);
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void delete_fails_when_unknown_email() {
+        assertThatThrownBy(() -> controller.deleteUser(""))
+                .isInstanceOf(UserNotFoundException.class);
     }
 }
