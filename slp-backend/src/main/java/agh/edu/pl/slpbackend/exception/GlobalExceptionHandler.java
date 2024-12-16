9268282b2a7d3ff9c57e580c2e58a0fc9b522db6
Witold Strzeboński <wstrzebo@gmail.com>
2024-12-17 00:20:06 +0100

    remove generic exception handler

--- slp-backend/src/main/java/agh/edu/pl/slpbackend/exception/GlobalExceptionHandler.java
+++ slp-backend/src/main/java/agh/edu/pl/slpbackend/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package agh.edu.pl.slpbackend.exception;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,10 +18,5 @@ public class GlobalExceptionHandler {
                 .stream().map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
         return ResponseEntity.badRequest().body(Map.of("errors", errors));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleGenericException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 }
